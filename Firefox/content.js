3528081d4a9199b7cb4a7bf5124b2d755ad5f5d1
Zinkelburger <andrewlbernal@gmail.com>
2023-08-06 09:00:42 -0400

    Added some css styling

--- Firefox/content.js
+++ Firefox/content.js
@@ -3,7 +3,7 @@ browser.storage.sync.get({ blocked: false }, function(items) {
         console.log('Received blocked value from chrome.storage:', items.blocked);
         // If blocked is true, update the page accordingly
         if (items.blocked === true && /https?:\/\/.*chess\.com\/(game|play\/online)/.test(window.location.href)) {
-                document.body.innerHTML = '<h1 style="color: white; background-color: black;">Chess.com has been blocked because you have exceeded the maximum number of games allowed.</h1>';
+                document.body.innerHTML = '<h1 style="color: white; background-color: black;">Chess.com has been blocked because you have exceeded the maximum number of losses allowed.</h1>';
         }
 });
    
@@ -14,7 +14,7 @@ browser.storage.onChanged.addListener(function(changes, namespace) {
                 console.log('Received blocked change:', changes.blocked.newValue);
                 // If so, update the page accordingly
                 if (changes.blocked.newValue === true && /https?:\/\/.*chess\.com\/(game|play\/online)/.test(window.location.href)) {
-                        document.body.innerHTML = '<h1 style="color: white; background-color: black;">Chess.com has been blocked because you have exceeded the maximum number of games allowed.</h1>';
+                        document.body.innerHTML = '<h1 style="color: white; background-color: black;">Chess.com has been blocked because you have exceeded the maximum number of losses allowed.</h1>';
                 }
         }
 });
